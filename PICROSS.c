cc8bb95d9d6615ff1272ba09e29c24f28616c5d4
Hitchiro93 <103576867+Hitchiro93@users.noreply.github.com>
2022-04-13 16:06:47 +0200

    Update PICROSS.c

--- PICROSS.c
+++ PICROSS.c
@@ -39,24 +39,27 @@ int L, C; /*L pour désigner la ligne et C la colonne du clic de la souris*/
 
 
 /**Initialisation de ncurses**/
-void ncurses_initialiser() {
-  initscr();	        /* Démarre le mode ncurses */
-  cbreak();	        /* Pour les saisies clavier (desac. mise en buffer) */
-  noecho();             /* Désactive l'affichage des caractères saisis */
-  keypad(stdscr, TRUE);	/* Active les touches spécifiques */
-  refresh();            /* Met a jour l'affichage */
-  curs_set(FALSE);      /* Masque le curseur */
+void ncurses_initialiser() 
+{
+  	initscr();	        /* Démarre le mode ncurses */
+  	cbreak();	        /* Pour les saisies clavier (desac. mise en buffer) */
+  	noecho();             /* Désactive l'affichage des caractères saisis */
+  	keypad(stdscr, TRUE);	/* Active les touches spécifiques */
+  	refresh();            /* Met a jour l'affichage */
+  	curs_set(FALSE);      /* Masque le curseur */
 }
 
 
 /**Initialisation des couleurs**/
-void ncurses_couleurs() {
+void ncurses_couleurs() 
+{
   /* Vérification du support de la couleur */
-  if(has_colors() == FALSE) {
-    endwin();
-    fprintf(stderr, "Le terminal ne supporte pas les couleurs.\n");
-    exit(EXIT_FAILURE);
-  }
+  	if(has_colors() == FALSE) 
+	{
+    		endwin();
+    		fprintf(stderr, "Le terminal ne supporte pas les couleurs.\n");
+    		exit(EXIT_FAILURE);
+  	}
 
   /* Activation des couleurs */
   	start_color();
@@ -71,46 +74,50 @@ void ncurses_couleurs() {
 }
 
 /**Initialisation de la souris**/
-void ncurses_souris() {
-  if(!mousemask(ALL_MOUSE_EVENTS, NULL)) {
-    endwin();
-    fprintf(stderr, "Erreur lors de l'initialisation de la souris.\n");
-    exit(EXIT_FAILURE);
-  }
+void ncurses_souris() 
+{
+  	if(!mousemask(ALL_MOUSE_EVENTS, NULL)) 
+	{
+		endwin();
+		fprintf(stderr, "Erreur lors de l'initialisation de la souris.\n");
+    		exit(EXIT_FAILURE);
+  	}
 
-  if(has_mouse() != TRUE) {
-    endwin();
-    fprintf(stderr, "Aucune souris n'est détectée.\n");
-    exit(EXIT_FAILURE);
-  }
+  	if(has_mouse() != TRUE) 
+	{
+    		endwin();
+    		fprintf(stderr, "Aucune souris n'est détectée.\n");
+    		exit(EXIT_FAILURE);
+  	}
 }
 
 
 /**Pour récupérer les coordonnées (x,y) du clic de la souris**/
-int click_souris()
+int click_souris(int nbLin,int nbCol)
 {
-  MEVENT event ;
-  int ch;
+  	MEVENT event ;
+  	int ch;
 
-  while((ch = getch()) != KEY_F(1)) 
-  {
-    switch(ch) 
-    {
-      case KEY_F(2): /*Pour quitter la boucle*/
-	return 1;
-      case KEY_MOUSE:
-        if(getmouse(&event) == OK) 
-	{
-	  C = event.x;
-	  L = event.y;
-	  if(event.bstate & BUTTON1_CLICKED)
-	  { 
-	    return 0;
-	  }
-	}
-    }
-  }
-  return 0;
+  	while((ch = getch()) != KEY_F(1)) 
+  	{
+    		switch(ch) 
+    		{
+      			case KEY_F(2): /*Pour quitter la boucle*/
+			return 1;
+      			case KEY_MOUSE:
+        		if(getmouse(&event) == OK) 
+			{
+	  			C = event.x;
+	  			L = event.y;	
+	  			if(event.bstate & BUTTON1_CLICKED)
+	  			{
+					if((L >= nbLin && L <= (nbLin * 2 + nbLin)) && (C > (nbCol - 3)*3 && C <= (nbCol * 3 + (nbCol - 3)*3)))
+	    					return 0;
+	  			}
+			}
+    		}
+  	}
+  	return 0;
 }
 
 
